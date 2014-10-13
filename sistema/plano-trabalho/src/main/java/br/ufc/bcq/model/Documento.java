44923b437d80e926997a1e05f6d348d3358513b1
Admin <Admin@200.129.38.236>
2014-10-13 20:11:26 -0300

    configurações da entidades.

--- sistema/plano-trabalho/src/main/java/br/ufc/bcq/model/Documento.java
+++ sistema/plano-trabalho/src/main/java/br/ufc/bcq/model/Documento.java
@@ -1,11 +1,47 @@
 package br.ufc.bcq.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Documento {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
+	@ManyToOne
 	private Atividade atividade;
 	
-	private String pathArquivo; 
+	private String pathArquivo;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Atividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(Atividade atividade) {
+		this.atividade = atividade;
+	}
+
+	public String getPathArquivo() {
+		return pathArquivo;
+	}
+
+	public void setPathArquivo(String pathArquivo) {
+		this.pathArquivo = pathArquivo;
+	} 
+	
+	
 
 }
