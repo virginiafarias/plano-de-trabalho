b10fc39a6a0550660566258a5c5f5cb68c6dfc3f
Admin <Admin@200.129.38.236>
2014-10-22 08:39:36 -0300

    Controle de acesso finalizado

--- sistema/plano-trabalho/src/main/java/br/ufc/bcq/controller/UsuarioController.java
+++ sistema/plano-trabalho/src/main/java/br/ufc/bcq/controller/UsuarioController.java
@@ -1,10 +1,63 @@
 package br.ufc.bcq.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("login")
 public class UsuarioController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Usuário e/ou senha inválidos!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+
+		model.addAttribute("error", "Usuário e/ou senha inválidos!");
+		return "login";
+
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model, HttpSession session) {
+		session.invalidate();
+
+		return "login";
+
+	}
+	
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public String acessoNegado(ModelMap model, Principal user) {
+		if (user != null) {
+			model.addAttribute("message", "Olá, " + user.getName() 
+			+ ", você não tem permissão para acessar essa página!");
+		} else {
+			model.addAttribute("message", 
+			"Você não tem permissão para acessar essa página!");
+		}
+		return "403";
+	}
 
 }
